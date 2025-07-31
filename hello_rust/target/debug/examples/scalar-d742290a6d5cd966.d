496aad80e9e6276b69cd6960c861b793319b8677
nasihudeen04@gmail.com <nasihudeen04@gmail.com>
2025-07-31 13:25:45 +0100

    Updated scalar and tuple exercises, add scalar example

---
+++ hello_rust/target/debug/examples/scalar-d742290a6d5cd966.d
@@ -0,0 +1,5 @@
+/Users/kanas/Desktop/web3/Polkadot/hello_rust/target/debug/examples/scalar-d742290a6d5cd966.d: examples/scalar.rs
+
+/Users/kanas/Desktop/web3/Polkadot/hello_rust/target/debug/examples/scalar-d742290a6d5cd966: examples/scalar.rs
+
+examples/scalar.rs:
---
+++ hello_rust/target/debug/examples/scalar.d
@@ -0,0 +1,1 @@
+/Users/kanas/Desktop/web3/Polkadot/hello_rust/target/debug/examples/scalar: /Users/kanas/Desktop/web3/Polkadot/hello_rust/examples/scalar.rs
